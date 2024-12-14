d7280862b131a47401d77d3d5fce6b066e436bc0
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2024-12-14 14:50:48 +0000

    ***Added*** apprail with navigation to application pages. ***Added*** dark and light mode and a mode switch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,33 @@
 <script lang="ts">
   import "../app.css";
-  import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+  import { AppRail, AppRailTile, AppRailAnchor, LightSwitch } from "@skeletonlabs/skeleton";
+  import "@fortawesome/fontawesome-free/css/all.min.css"
   let currentTile: number = 0;
 </script>
 
-<AppRail class="fixed top-0 left-0 h-full w-14 border-r border-surface-200 z-20">
+<AppRail class="fixed top-0 left-0 h-full w-18  surface-200 z-20">
   <svelte:fragment slot="lead">
-    <AppRailAnchor href="/"> (icon) </AppRailAnchor>
-    <AppRailAnchor href="/" target="_blank" title="Account">
-      <i class="devicon-github-original icon"></i>
+    <AppRailAnchor href="/">
+      <i class="fa-solid fa-house text-xl"></i>
     </AppRailAnchor>
-    <AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+    <AppRailAnchor href="/" target="_blank" title="Account">
+      <i class="fa-solid fa-computer text-xl"></i>
+    </AppRailAnchor>
+    <AppRailAnchor href="/" target="_blank" title="Account">
+      <i class="fa-solid fa-user text-xl"></i>
+    </AppRailAnchor>
+    <AppRailAnchor href="/" target="_blank" title="Account">
+      <i class="fa-solid fa-comments text-xl"></i>
+    </AppRailAnchor>
   </svelte:fragment>
+
   <svelte:fragment slot="trail">
-    <AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+    <AppRailAnchor href="/" target="_blank" title="Account">
+      <i class="fa-solid fa-gear text-xl"></i>
+    </AppRailAnchor>
+    <div class="container pl-4 pb-4">
+      <LightSwitch />
+    </div>
   </svelte:fragment>
 </AppRail>
 
