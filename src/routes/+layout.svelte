35efb153384ccdd1cbe149851c1bf7b0e7fa99e8
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2024-12-14 14:27:20 +0000

    First commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import "../app.css";
+  import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+  let currentTile: number = 0;
+</script>
+
+<AppRail class="fixed top-0 left-0 h-full w-14 border-r border-surface-200 z-20">
+  <svelte:fragment slot="lead">
+    <AppRailAnchor href="/"> (icon) </AppRailAnchor>
+    <AppRailAnchor href="/" target="_blank" title="Account">
+      <i class="devicon-github-original icon"></i>
+    </AppRailAnchor>
+    <AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+  </svelte:fragment>
+  <svelte:fragment slot="trail">
+    <AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+  </svelte:fragment>
+</AppRail>
+
+<main class="ml-14 p-4">
+  <slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+  let name = $state("");
+  let greetMsg = $state("");
+
+</script>
+
+<main class="container">
+  <h1 class="h1 p-24">Welcome Adrian</h1>
+  
+</main>
+
+
